80a121a0d0fb3e1fe7693e912428539d2582ed00
Robbie <hobe6590@gmail.com>
2026-04-20 18:19:42 -0700

    simplified calculator code

--- my_first_calculator.py
+++ my_first_calculator.py
@@ -1,26 +1,37 @@
 # my_first_calculator.py by AceLewis
 # TODO: Make it work for all floating point numbers too
 
+import math
+
 if 3/2 == 1:  # Because Python 2 does not know maths
     input = raw_input  # Python 2 compatibility
 
 print('Welcome to this calculator!')
-print('It can add, subtract, multiply and divide whole numbers from 0 to 50')
-num1 = int(input('Please choose your first number: '))
-sign = input('What do you want to do? +, -, /, or *: ')
-num2 = int(input('Please choose your second number: '))
+print('It can add, subtract, multiply, divide, find square roots, and calculate exponentials for whole numbers from 0 to 50')
+sign = input('What do you want to do? +, -, /, *, sqrt, or ^: ')
 
-if sign == '+':
-    result = num1 + num2
-    print(f"{num1}+{num2} = {result}")
-elif sign == '-':
-    result = num1 - num2
-    print(f"{num1}-{num2} = {result}")
-elif sign == '*':
-    result = num1 * num2
-    print(f"{num1}*{num2} = {result}")
-elif sign == '/':
-    result = num1 / num2
-    print(f"{num1}/{num2} = {result}")
+if sign == 'sqrt':
+    num1 = int(input('Please choose your number: '))
+    result = math.sqrt(num1)
+    print(f"√{num1} = {result}")
+else:
+    num1 = int(input('Please choose your first number: '))
+    num2 = int(input('Please choose your second number: '))
+
+    if sign == '+':
+        result = num1 + num2
+        print(f"{num1}+{num2} = {result}")
+    elif sign == '-':
+        result = num1 - num2
+        print(f"{num1}-{num2} = {result}")
+    elif sign == '*':
+        result = num1 * num2
+        print(f"{num1}*{num2} = {result}")
+    elif sign == '/':
+        result = num1 / num2
+        print(f"{num1}/{num2} = {result}")
+    elif sign == '^':
+        result = num1 ** num2
+        print(f"{num1}^{num2} = {result}")
 
 print("Thanks for using this calculator, goodbye :)")
